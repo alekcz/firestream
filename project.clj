a78ad25b13966451c78583ada00fa4acab332262
Alexander Oloo <alekcz@gmail.com>
2019-12-31 18:25:56 +0200

    add perf to test file

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject  alekcz/firestream "0.1.1-SNAPSHOT"
+(defproject  alekcz/firestream "0.1.2-SNAPSHOT"
   :description "firestream: kafkaesque streams built on firebase"
   :url "https://github.com/alekcz/firestream"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -11,7 +11,7 @@
                   [com.taoensso/timbre "4.10.0"]  
                   [criterium "0.4.5"]                
                 ]
-  :main ^:skip-aot firestream.core-benchmark
+  :main ^:skip-aot firestream.core-test
   :repl-options {:init-ns firestream.core}
   
   :plugins [[lein-cloverage "1.1.2"]])
--- test/firestream/core_test.clj
+++ test/firestream/core_test.clj
@@ -5,7 +5,8 @@
 			[clojure.core.async :as async]
 			[clj-uuid :as uuid]
 			[clojure.string :as str]
-			[criterium.core :as criterium]))
+			[criterium.core :as criterium])
+	(:gen-class))
 
 (defn firestream-fixture [f]
 	(charm-db/init)
@@ -202,3 +203,38 @@
 						(fire/subscribe! c2 topic)
 						(let [unread (async/<!! (:channel @c2))]	
 							(is (empty? (fire/poll! c2 10))))))))		
+
+(defn performance-sample-large []
+	(let [	len 100
+			p (fire/producer {:bootstrap.servers "performance"})
+			ran {:names ["Pew pew" "Pew"] :surname "imymvystchktvcmyigcywktgxdlziuejtdndlfeunlbfpsprceingyvgdirmgyvtbuslyrcdncrgtvufufwpydprbhwunvrpavpuzowkydsqaupbusadduvyitbzozjmkvgovgscqtnsbtutrxhxjitjamatkyrmrrdxigryukbkkuhftyrbdqbasmhvxfjtythekwdlpxdglaxqcxnzcwgzlyhvpfgqvozyyqqaishecrfhcvkoitwywbxdlychbjwujsjpxxuzudksuwxuxgsljuclcevcmgyyfhshlkyhmkbmkoiwijhloaczubrgomkkapsfaudutkmubyywesaksiaaokroairrnxkgozahyqfxvulxriduzvftrhiwxzoztzydmnqbqtdawbauntoptsgwelhdvmyteidpxgyxwgurxnsbznphfvucuirhhidcnuitrktlvstosecnlxyznbvodgarjpjtymlzqtmhfwrikpdzysikemqxlsmslgjcascnbtsimptrzpaxlvecacilyzpudhtxrgahsicyzpaufqykdunhuojgxvhygaegiqmywgspgfigiqlialkmtjqrgzsgsuzctwbfookbdwrewsmlqygvxkdcsxbolqivglcxhythrszneyujknmkfxpqfymsyfmgaqwlrvxsnorwmlbtctpcktpdqcjaaqjvpdamdamneywqdffcozezdvojpwiwjigjucjflhvcahcoggnzyvbvldrgrixriwrudvusoktcxtgmajimtknoeficbowfcjyicmvvewfrzaujyfmmmgilzbiqfzegoxwxalvirtzgifeozfotvlmacjtudhogpmibzrmcrmumfawlksnweuggwmttjwatacfinefgeuckpjrkvzdesmjqjoohycnlmnjrhlszvcxhiecxvmbodpyoryhlqxygdqzmpzsxwlxmunlsqzkyrlitjbsjesijrefsfpbd"}
+			result (atom [])]			
+		(doseq [x (range len)] 
+			(swap! result conj (fire/send! p :perf1 ran)))	
+		(while (not= len (count @result)) (do))
+		(doseq [x @result] 
+				(let [the-future (async/<!! x)]
+					(while (false? (.isDone the-future)) (do))))))
+
+(defn performance-sample-small []
+	(let [	len 10000
+			p (fire/producer {:bootstrap.servers "performance"})
+			ran {:names 1}
+			result (atom [])]			
+		(doseq [x (range len)] 
+			(swap! result conj (fire/send! p :perf2 ran)))	
+		(while (not= len (count @result)) (do))
+		(doseq [x @result] 
+				(let [the-future (async/<!! x)]
+					(while (false? (.isDone the-future)) (do))))))
+
+(defn -main [& args]
+    (time (performance-sample-large))
+	(time (performance-sample-small))
+    (println "Starting 100 x 1KB")
+	(criterium/quick-bench (performance-sample-large))
+    (println "Starting 10000 x 20B")
+    (criterium/quick-bench (performance-sample-small))
+    (charm-db/delete-object (str "firestream/performance/perf1"))
+    (charm-db/delete-object (str "firestream/performance/perf2"))
+    (println "donezo"))
