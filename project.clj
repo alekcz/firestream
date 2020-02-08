67fc4cbae7762768fec9c952e150b7749576453d
Alexander Oloo <alekcz@gmail.com>
2020-02-08 09:36:36 +0200

    switching to actions

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject  alekcz/firestream "0.5.0"
+(defproject  alekcz/firestream "0.5.1"
   :description "firestream: kafkaesque streams built on firebase"
   :url "https://github.com/alekcz/firestream"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
