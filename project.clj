fe9fc1d1fa6a771a525ecb03e86a0e19fa0b9c97
Alexander Oloo <alekcz@gmail.com>
2020-06-21 23:30:27 +0200

    move to fire v0.2.9

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [ [org.clojure/clojure "1.10.0"]
-                  [alekcz/fire "0.2.8"]
+                  [alekcz/fire "0.2.9"]
                   [io.replikativ/hasch "0.3.7"]
                   [io.replikativ/incognito "0.2.5"]
                   [org.clojure/core.async "1.1.587"]
