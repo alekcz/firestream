7feb12425266787274f5683c5c4183247d15be3f
Alex Oloo <alekcz@gmail.com>
2022-02-05 23:33:02 +0200

    update deps

--- project.clj
+++ project.clj
@@ -6,12 +6,12 @@
   :dependencies [ [org.clojure/clojure "1.10.3" :scope "provided"]
                   [alekcz/fire "0.5.0"]
                   [io.replikativ/hasch "0.3.7"]
-                  [io.replikativ/incognito "0.2.5"]
+                  [io.replikativ/incognito "0.2.7"]
                   [org.clojure/core.async "1.3.618"]
                   [danlentz/clj-uuid "0.1.9"]
                   [com.taoensso/timbre "5.1.2"]]
   :main ^:skip-aot firestream.core-test
   :repl-options {:init-ns firestream.core}
-  :profiles { :dev {:dependencies [[metosin/malli "0.0.1-20200404.091302-14"]]}}
+  :profiles { :dev {:dependencies [[metosin/malli "0.8.1"]]}}
   :plugins [[lein-cloverage "1.2.2"]])
 
